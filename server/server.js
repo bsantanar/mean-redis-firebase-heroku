9be0df1d04f20d2be28b5cffd45410c581d527b7
unknown <bastian.santana@usach.cl>
2020-08-23 14:48:43 -0400

    public folder

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('./config/config');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 
@@ -9,7 +10,14 @@ const port = process.env.PORT || config.PORT;
 
 app.use(cors());
 
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api/products', products);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+  });
 
 app.listen(port, () => {
     console.log(`Server initialized at port ${port}`);
