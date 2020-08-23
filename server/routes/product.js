d979e1cb007c92bc6b6892507dd418feda42a911
unknown <bastian.santana@usach.cl>
2020-08-23 17:10:09 -0400

    ripley error

--- server/routes/product.js
+++ server/routes/product.js
@@ -17,7 +17,7 @@ router.get('/bySku/:key', authMiddleware.checkToken, cacheMiddleware.cache, asyn
         await db.setex(sku, JSON.stringify(data));
         return res.status(200).json({ ok:true, message: 'Fetched from ripley api', data});
     } catch (e) {
-        return res.status(500).json({ok: false, message: 'Internal server Error', data: e.data})
+        return res.status(500).json({ok: false, message: 'Internal server Error', data: e.response.data})
     }
 });
 
@@ -33,7 +33,7 @@ router.get('/:key', authMiddleware.checkToken, cacheMiddleware.cache, async (req
         await db.setex(products, JSON.stringify(data));
         return res.status(200).json({ ok:true, message: 'Fetched from ripley api', data});
     } catch (e) {
-        return res.status(500).json({ok: false, message: 'Internal server Error', data: e.data})
+        return res.status(500).json({ok: false, message: 'Internal server Error', data: e.response.data})
     }
 });
 
